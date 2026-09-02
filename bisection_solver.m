2c12414e5fadf21ec5a7f9b76ac18bc25585937c
Liam Carlin <liamzcarlin@gmail.com>
2026-09-02 13:34:34 -0400

    three solving methods done

--- bisection_solver.m
+++ bisection_solver.m
@@ -11,5 +11,10 @@
 %   x: estimate for root of fun
 %   exit_flag: an integer indicating whether or not the solver succeeded
 function [x, exit_flag] = bisection_solver(fun,x_left,x_right,dxtol,ftol,max_iter)
-    %your code here
+    while dxtol < max_iter
+        if(sign(x_left) == sign(x_right))
+            x_midpoint = (x_left+x_right)/2;
+    
+            
+    end
 end
